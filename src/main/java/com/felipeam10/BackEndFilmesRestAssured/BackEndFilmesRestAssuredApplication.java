8458151b5f5b01f160ef14f73b5259f9c744580a
felipeam10 <felipeam10@hotmail.com>
2024-12-12 17:32:31 -0300

    first commit

---
+++ src/main/java/com/felipeam10/BackEndFilmesRestAssured/BackEndFilmesRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.felipeam10.BackEndFilmesRestAssured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndFilmesRestAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndFilmesRestAssuredApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipeam10/BackEndFilmesRestAssured/BackEndFilmesRestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipeam10.BackEndFilmesRestAssured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndFilmesRestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
