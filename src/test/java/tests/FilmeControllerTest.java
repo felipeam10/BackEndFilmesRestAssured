34c4c18af9ecd6247dd6a52bed7ae0974d7b7ecf
felipeam10 <felipeam10@hotmail.com>
2025-01-03 11:48:29 -0300

    Refactoring project using junit asserts

--- src/test/java/tests/FilmeControllerTest.java
+++ src/test/java/tests/FilmeControllerTest.java
@@ -1,14 +1,20 @@
 package tests;
 
 import io.restassured.http.ContentType;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import support.utils.BaseUrlSetup;
 import support.utils.RestAssuredConfig;
 import support.enums.Endpoint;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import io.restassured.response.*;
+
+import java.util.List;
+import java.util.Map;
 
 public class FilmeControllerTest {
 
@@ -22,18 +28,19 @@ public class FilmeControllerTest {
 
         int codigoFilme = Integer.parseInt(BaseUrlSetup.getProperty("codigo.filme"));
 
+        Response response =
         given()
             .pathParam("codigo", codigoFilme)
         .when()
-            .get(Endpoint.FILME.getPath())
-        .then()
-            .statusCode(200)
-            .contentType(ContentType.JSON)
-            .body("codigo", equalTo(codigoFilme))
-            .body("nome", notNullValue())
-            .body("sinopse", notNullValue())
-            .body("faixaEtaria", notNullValue())
-            .body("genero", notNullValue());
+            .get(Endpoint.FILME.getPath());
+
+        assertEquals(200, response.getStatusCode());
+        assertEquals(ContentType.JSON.toString(), response.getContentType());
+        assertEquals(codigoFilme, response.jsonPath().getInt("codigo"));
+        assertNotNull(response.jsonPath().getString("nome"));
+        assertNotNull(response.jsonPath().getString("sinopse"));
+        assertNotNull(response.jsonPath().getString("faixaEtaria"));
+        assertNotNull(response.jsonPath().getString("genero"));
     }
 
     @Test
@@ -41,42 +48,46 @@ public class FilmeControllerTest {
 
         int codigoFilmeInvalido = Integer.parseInt(BaseUrlSetup.getProperty("codigo.filmeInvalido"));
 
+        Response response =
         given()
             .pathParam("codigo", codigoFilmeInvalido)
         .when()
-            .get(Endpoint.FILME.getPath())
-        .then()
-            .statusCode(404);
+            .get(Endpoint.FILME.getPath());
+
+        assertEquals(404, response.getStatusCode());
     }
 
     @Test
     public void testConsultarTodosOsFilmes_Success() {
 
+        Response response =
         given()
         .when()
-            .get(Endpoint.FILMES.getPath())
-        .then()
-            .statusCode(200) // Expect HTTP 200 OK
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("$", not(empty())) // Ensure the response is not empty
-            .body("size()", greaterThan(0)) // Ensure at least one movie is returned
-            .body("[0].codigo", notNullValue()) // Validate the first movie has an ID
-            .body("[0].nome", notNullValue()) // Validate the first movie has a name
-            .body("[0].sinopse", notNullValue()) // Validate the first movie has a description
-            .body("[0].faixaEtaria", notNullValue()) // Validate the first movie has a description
-            .body("[0].genero", notNullValue()) // Validate the first movie has a description
-        ;
+            .get(Endpoint.FILMES.getPath());
+
+        assertEquals(200, response.getStatusCode());
+        assertEquals(ContentType.JSON.toString(), response.getContentType());
+        List<?> filmes = response.jsonPath().getList("$");
+        assertNotNull(filmes);
+        assertTrue(filmes.size() > 0);
+
+        Map<String, Object> primeiroFilme = response.jsonPath().getMap("[0]");
+        assertNotNull(primeiroFilme.get("codigo"));
+        assertNotNull(primeiroFilme.get("nome"));
+        assertNotNull(primeiroFilme.get("sinopse"));
+        assertNotNull(primeiroFilme.get("faixaEtaria"));
+        assertNotNull(primeiroFilme.get("genero"));
     }
 
     @Test
     public void testConsultarTodosOsFilmes_InvalidEndpoint() {
 
-        // Test for an invalid endpoint
+        Response response =
         given()
         .when()
-            .get(Endpoint.FILMES_INVALID.getPath()) // Invalid endpoint
-        .then()
-            .statusCode(404); // Expect HTTP 404 Not Found
+            .get(Endpoint.FILMES_INVALID.getPath());
+
+        assertEquals(404, response.getStatusCode(), "Expected HTTP 404 Not Found");
     }
 
     @Test
@@ -84,30 +95,34 @@ public class FilmeControllerTest {
 
         String filmeJson = BaseUrlSetup.getProperty("filme.success");
 
+        Response response =
         given()
             .contentType(ContentType.JSON)
             .body(filmeJson)
         .when()
-            .post(Endpoint.SALVAR.getPath())
-        .then()
-            .statusCode(201) // Expect HTTP 201 Created
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("codigo", equalTo(10)) // Validate the returned movie's code
-            .body("nome", equalTo("Filme de Ação")) // Validate the movie's name
-            .body("genero", equalTo("Ação")) // Validate the movie's genre
-            .body("faixaEtaria", equalTo("12")) // Validate the movie's age rating
-            .body("sinopse", equalTo("Um filme emocionante de ação.")); // Validate the movie's synopsis
+            .post(Endpoint.SALVAR.getPath());
+
+        assertEquals(201, response.getStatusCode(), "Expected HTTP 201 Created");
+
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+
+        assertEquals(10, response.jsonPath().getInt("codigo"), "Expected movie code to be 10");
+        assertEquals("Filme de Ação", response.jsonPath().getString("nome"), "Expected movie name to be 'Filme de Ação'");
+        assertEquals("Ação", response.jsonPath().getString("genero"), "Expected movie genre to be 'Ação'");
+        assertEquals("12", response.jsonPath().getString("faixaEtaria"), "Expected movie age rating to be '12'");
+        assertEquals("Um filme emocionante de ação.", response.jsonPath().getString("sinopse"), "Expected movie synopsis to match");
     }
 
     @Test
     public void testDeletarFilmePorId_Success() {
 
+        Response response =
         given()
             .pathParam("codigo", 10)
         .when()
-            .delete(Endpoint.FILME.getPath())
-        .then()
-            .statusCode(200); // Expect HTTP 204 No Content
+            .delete(Endpoint.FILME.getPath());
+
+        assertEquals(200, response.getStatusCode(), "Expected HTTP 200 OK");
     }
 
     @Test
@@ -115,16 +130,16 @@ public class FilmeControllerTest {
 
         String filmeJson = BaseUrlSetup.getProperty("filme.conflict");
 
-        // Assuming the movie with codigo 1 already exists
+        Response response =
         given()
             .contentType(ContentType.JSON)
             .body(filmeJson)
         .when()
-            .post(Endpoint.SALVAR.getPath())
-        .then()
-            .statusCode(409) // Expect HTTP 409 Conflict
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("message", equalTo("Filme já cadastrado!")); // Validate the conflict message
+            .post(Endpoint.SALVAR.getPath());
+
+        assertEquals(409, response.getStatusCode(), "Expected HTTP 409 Conflict");
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+        assertEquals("Filme já cadastrado!", response.jsonPath().getString("message"), "Expected conflict message to match");
     }
 
     @Test
@@ -132,15 +147,16 @@ public class FilmeControllerTest {
 
         String filmeJson = BaseUrlSetup.getProperty("filme.sem.faixa.etaria");
 
+        Response response =
         given()
             .contentType(ContentType.JSON)
             .body(filmeJson)
         .when()
-            .post(Endpoint.SALVAR.getPath())
-        .then()
-            .statusCode(400) // Expect HTTP 400 Bad Request
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("message", equalTo("Faixa etária é obrigatória!")); // Validate the error message
+            .post(Endpoint.SALVAR.getPath());
+
+        assertEquals(400, response.getStatusCode(), "Expected HTTP 400 Bad Request");
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+        assertEquals("Faixa etária é obrigatória!", response.jsonPath().getString("message"), "Expected error message to match");
     }
 
     @Test
@@ -148,15 +164,16 @@ public class FilmeControllerTest {
 
         String filmeJson = BaseUrlSetup.getProperty("filme.sem.genero");
 
+        Response response =
         given()
             .contentType(ContentType.JSON)
             .body(filmeJson)
         .when()
-            .post(Endpoint.SALVAR.getPath())
-        .then()
-            .statusCode(400) // Expect HTTP 400 Bad Request
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("message", equalTo("Genêro é obrigatório!")); // Validate the error message
+            .post(Endpoint.SALVAR.getPath());
+
+        assertEquals(400, response.getStatusCode(), "Expected HTTP 400 Bad Request");
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+        assertEquals("Genêro é obrigatório!", response.jsonPath().getString("message"), "Expected error message to match");
     }
 
     @Test
@@ -164,15 +181,16 @@ public class FilmeControllerTest {
 
         String filmeJson = BaseUrlSetup.getProperty("filme.sem.nome");
 
+        Response response =
         given()
             .contentType(ContentType.JSON)
             .body(filmeJson)
         .when()
-            .post(Endpoint.SALVAR.getPath())
-        .then()
-            .statusCode(400) // Expect HTTP 400 Bad Request
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("message", equalTo("Nome é obrigatório!")); // Validate the error message
+            .post(Endpoint.SALVAR.getPath());
+
+        assertEquals(400, response.getStatusCode(), "Expected HTTP 400 Bad Request");
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+        assertEquals("Nome é obrigatório!", response.jsonPath().getString("message"), "Expected error message to match");
     }
 
     @Test
@@ -180,28 +198,30 @@ public class FilmeControllerTest {
 
         String filmeJson = BaseUrlSetup.getProperty("filme.sem.sinopse");
 
+        Response response =
         given()
             .contentType(ContentType.JSON)
             .body(filmeJson)
         .when()
-            .post(Endpoint.SALVAR.getPath())
-        .then()
-            .statusCode(400) // Expect HTTP 400 Bad Request
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("message", equalTo("Sinopse é obrigatório!")); // Validate the error message
+            .post(Endpoint.SALVAR.getPath());
+
+        assertEquals(400, response.getStatusCode(), "Expected HTTP 400 Bad Request");
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+        assertEquals("Sinopse é obrigatório!", response.jsonPath().getString("message"), "Expected error message to match");
     }
 
     @Test
     public void testDeletarFilmePorId_NotFound() {
         // Assuming a movie with codigo 999 does not exist in the database
+        Response response =
         given()
             .pathParam("codigo", 999)
         .when()
-            .delete(Endpoint.FILME.getPath())
-        .then()
-            .statusCode(404) // Expect HTTP 404 Not Found
-            .contentType(ContentType.JSON) // Expect JSON response
-            .body("error", equalTo("Not Found")); // Validate the error message
+            .delete(Endpoint.FILME.getPath());
+
+        assertEquals(404, response.getStatusCode(), "Expected HTTP 404 Not Found");
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+        assertEquals("Not Found", response.jsonPath().getString("error"), "Expected error message to match");
     }
 
     @Test
@@ -209,19 +229,20 @@ public class FilmeControllerTest {
 
         String filmeJson = BaseUrlSetup.getProperty("editar.filme.success");
 
+        Response response =
         given()
             .pathParam("codigo", 1)
             .contentType(ContentType.JSON)
             .body(filmeJson)
         .when()
-            .put(Endpoint.FILME.getPath())
-        .then()
-            .statusCode(200) // Expect HTTP 200 OK
-            .contentType(ContentType.JSON)
-            .body("nome", equalTo("Novo Nome do Filme"))
-            .body("sinopse", equalTo("Nova sinopse do filme"))
-            .body("genero", equalTo("Ação"))
-            .body("faixaEtaria", equalTo("16+"));
+            .put(Endpoint.FILME.getPath());
+
+        assertEquals(200, response.getStatusCode(), "Expected HTTP 200 OK");
+        assertEquals(ContentType.JSON.toString(), response.getContentType(), "Expected JSON response");
+        assertEquals("Novo Nome do Filme", response.jsonPath().getString("nome"), "Expected movie name to match");
+        assertEquals("Nova sinopse do filme", response.jsonPath().getString("sinopse"), "Expected movie synopsis to match");
+        assertEquals("Ação", response.jsonPath().getString("genero"), "Expected movie genre to match");
+        assertEquals("16+", response.jsonPath().getString("faixaEtaria"), "Expected movie age rating to match");
     }
 
     @Test
